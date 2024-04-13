29da5a3ff236ee8e3743cbd7ddafa2c5b1a82bf0
GATETE KATO BRUNO <64279605+Gatete-Bruno@users.noreply.github.com>
2024-04-13 21:32:55 +0200

    terraform configs

--- humangov-terraform/resources.tf
+++ humangov-terraform/resources.tf
@@ -1,4 +1,12 @@
+
+
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
+}
+
+# Create a local file named notes.txt
+resource "local_file" "notes" {
+  content  = "This file is created by Terraform"
+  filename = "notes.txt"
 }
