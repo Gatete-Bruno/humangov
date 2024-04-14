21a8fd55a1ad7d78b768e8823776fd6f06297511
GATETE KATO BRUNO <64279605+Gatete-Bruno@users.noreply.github.com>
2024-04-14 22:57:11 +0200

    Daily Stuff

--- humangov-terraform/resources.tf
+++ humangov-terraform/resources.tf
@@ -10,3 +10,20 @@ resource "local_file" "notes" {
   content  = "This file is created by Terraform"
   filename = "notes.txt"
 }
+
+
+resource "random_string" "bucket_name_suffix" {
+    length = 5
+    special = false
+    upper = false
+}
+
+
+resource "aws_s3_bucket" "HG1_suffix" {
+    bucket = "humangov-tf-test-bucket1-${random_string.bucket_name_suffix.result}"
+
+    tags = {
+        Application = var.application
+        ProjectId = var.projectid
+    }
+}
