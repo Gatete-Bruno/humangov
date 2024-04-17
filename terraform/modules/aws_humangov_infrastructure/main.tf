094b04a7a3f697ff5828a9f8992e3977559bb007
GATETE KATO BRUNO <64279605+Gatete-Bruno@users.noreply.github.com>
2024-04-17 21:43:20 +0200

    Terraform Provisioning

--- terraform/modules/aws_humangov_infrastructure/main.tf
+++ terraform/modules/aws_humangov_infrastructure/main.tf
@@ -22,10 +22,25 @@ resource "aws_security_group" "state_ec2_sg" {
 }
 
 resource "aws_instance" "state_ec2" {  
-  ami           = "ami-0230bd60aa48260c6"  
+  ami           = "ami-04e5276ebb8451442"  #AMI for our region 
   instance_type = "t2.micro"  
   key_name = "humangov-ec2-key"  
   vpc_security_group_ids = [aws_security_group.state_ec2_sg.id]  
+  
+  tags = {  
+    Name = "humangov-${var.state_name}"  
+  }  
+}
+
+resource "aws_dynamodb_table" "state_dynamodb" {  
+  name           = "humangov-${var.state_name}-dynamodb"  
+  billing_mode   = "PAY_PER_REQUEST"  
+  hash_key       = "id"  
+  
+  attribute {  
+    name = "id"  
+    type = "S" #Type String  
+  }  
   
   tags = {  
     Name = "humangov-${var.state_name}"  
