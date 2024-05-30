9a16f73fea4e46188c8548189288187699b7ba42
GATETE KATO BRUNO <64279605+Gatete-Bruno@users.noreply.github.com>
2024-05-30 21:38:45 +0200

    aws & gcp

---
+++ aws-humangov/terraform/modules/aws_humangov_infrastructure/main.tf
@@ -0,0 +1,136 @@
+resource "aws_security_group" "state_ec2_sg" {
+  name        = "humangov-${var.state_name}-ec2-sg"
+  description = "Allow traffic on ports 22, 80, and 5000"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80 # HTTP port, NGINX port to access to the HumanGov
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {  # Port for Python Flask Application, optional, we may use for troubleshooting. Users will reach the application via NGINX port and pass the request to the Flask Application as we explained before.
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress { # This is the port allowing EC2 instance connecting to the internet and download packages required for our application like NGINX.
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "humangov-${var.state_name}"
+  }
+}
+
+resource "aws_instance" "state_ec2" {  
+    ami = "ami-007855ac798b5175e"  
+    instance_type = "t2.micro"  
+    key_name = "humangov-ec2-key"  
+    vpc_security_group_ids = [aws_security_group.state_ec2_sg.id]  
+    iam_instance_profile = aws_iam_instance_profile.s3_dynamodb_full_access_instance_profile.name  
+    
+    # Append EC2 instance information to the /etc/ansible/hosts file, enabling Ansible to manage the instance.  
+   provisioner "local-exec" {
+       command = "sudo sh -c 'echo ${var.state_name} id=${self.id} ansible_host=${self.private_ip} ansible_user=ubuntu us_state=${var.state_name} aws_region=${var.region} aws_s3_bucket=${aws_s3_bucket.state_s3.bucket} aws_dynamodb_table=${aws_dynamodb_table.state_dynamodb.name} >> /etc/ansible/hosts'"
+}
+
+    # Remove the information about the EC2 instance from the /etc/ansible/hosts file when the instance is destroyed.  
+    provisioner "local-exec" {  
+        when = destroy
+        command = "sed -i '/${self.id}/d' /etc/ansible/hosts"  
+    }  
+      
+    tags = {  
+        Name = "humangov-${var.state_name}"  
+    }  
+}
+
+  
+
+resource "aws_dynamodb_table" "state_dynamodb" {
+  name         = "humangov-${var.state_name}-dynamodb"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S" # Type String
+  }
+
+  tags = {
+    Name = "humangov-${var.state_name}"
+  }
+}
+
+resource "random_string" "bucket_suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket" "state_s3" {
+  bucket = "humangov-${var.state_name}-s3-${random_string.bucket_suffix.result}"
+
+  tags = {
+    Name = "humangov-${var.state_name}"
+  }
+}
+
+resource "aws_iam_role" "s3_dynamodb_full_access_role" {
+  name = "humangov-${var.state_name}-s3_dynamodb_full_access_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+    Name = "humangov-${var.state_name}"
+  }
+
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full_access_role_policy_attachment" {
+  role       = aws_iam_role.s3_dynamodb_full_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_full_access_role_policy_attachment" {
+  role       = aws_iam_role.s3_dynamodb_full_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+
+}
+
+resource "aws_iam_instance_profile" "s3_dynamodb_full_access_instance_profile" {
+  name = "humangov-${var.state_name}-s3_dynamodb_full_access_instance_profile"
+  role = aws_iam_role.s3_dynamodb_full_access_role.name
+
+  tags = {
+    Name = "humangov-${var.state_name}"
+  }
+}
